19bdc9109a095bfbc8a9c25f22e05d54f406cd75
Yvan Pangue <yvanbinda@gmail.com>
2026-01-13 13:59:54 +0100

    fix(dependency_injection): fix improper getx imports

--- lib/pages/home/widgets/header.dart
+++ lib/pages/home/widgets/header.dart
@@ -11,7 +11,7 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 16),
       child: Row(
         children: [
           if (showMenu)
